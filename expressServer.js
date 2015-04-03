a6aee6417d8a94ed2b1dcdf607ea68d78e6ccc2f
Daniel Burt <daniel.burt@gmail.com>
2015-04-02 20:52:01 -0600

    ...

--- expressServer.js
+++ expressServer.js
@@ -25,11 +25,10 @@ var app = express();
 
 http.createServer(app).listen(80);
 
-app.get('/', express.static('home.html');
+app.get('/', express.static('views/home.html');
 
 app.post('/chat', function(req, res){
 	console.log("In POST chat route");
-
 	var MongoClient = require('mongodb').MongoClient;
 	MongoClient.connect("mongodb:localhost:/awesomeDB", function(err, db){
 		if(err) throw err;
@@ -40,5 +39,20 @@ app.post('/chat', function(req, res){
 	res.status(200);
 	res.end();
 });
+
+//Returns an array with 4 hard coded users for tesing purposes:
+app.get('/hardcodedusers', function(req, res){
+	userArr = ["Daniel", "Landen", "Matt", "Devin"];
+	res.json(userArr);
+	res.status(200);
+	res.end();
+});
+
+
+
+
+
+
+
 
 
