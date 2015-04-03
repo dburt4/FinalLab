0e0e18c7abbb7691a0ee1bbfb0a8a21ce34bfd6c
Daniel Burt <daniel.burt@gmail.com>
2015-04-02 20:45:09 -0600

    updates

--- expressServer.js
+++ expressServer.js
@@ -1,3 +1,22 @@
+/*
+                                                                  
+                                                                  
+  .--.--.       ,---,.,-.----.                  ,---,.,-.----.    
+ /  /    '.   ,'  .' |\    /  \        ,---.  ,'  .' |\    /  \   
+|  :  /`. / ,---.'   |;   :    \      /__./|,---.'   |;   :    \  
+;  |  |--`  |   |   .'|   | .\ : ,---.;  ; ||   |   .'|   | .\ :  
+|  :  ;_    :   :  |-,.   : |: |/___/ \  | |:   :  |-,.   : |: |  
+ \  \    `. :   |  ;/||   |  \ :\   ;  \ ' |:   |  ;/||   |  \ :  
+  `----.   \|   :   .'|   : .  / \   \  \: ||   :   .'|   : .  /  
+  __ \  \  ||   |  |-,;   | |  \  ;   \  ' .|   |  |-,;   | |  \  
+ /  /`--'  /'   :  ;/||   | ;\  \  \   \   ''   :  ;/||   | ;\  \ 
+'--'.     / |   |    \:   ' | \.'   \   `  ;|   |    \:   ' | \.' 
+  `--'---'  |   :   .':   : :-'      :   \ ||   :   .':   : :-'   
+            |   | ,'  |   |.'         '---" |   | ,'  |   |.'     
+            `----'    `---'                 `----'    `---'       
+                                                                  
+
+*/
 var express = require('express');
 var http = require('http');
 var fs = require('fs');
@@ -6,8 +25,20 @@ var app = express();
 
 http.createServer(app).listen(80);
 
-app.get('/', express.static('index.html');
+app.get('/', express.static('home.html');
 
+app.post('/chat', function(req, res){
+	console.log("In POST chat route");
 
+	var MongoClient = require('mongodb').MongoClient;
+	MongoClient.connect("mongodb:localhost:/awesomeDB", function(err, db){
+		if(err) throw err;
+		db.collection('chat').insert(req.body, function(err, records){
+			console.log("record inserted as " + records[0]._id);
+		});
+	});
+	res.status(200);
+	res.end();
+});
 
 
